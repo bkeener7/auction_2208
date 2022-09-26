a2e521768170d8907124fbd6b6e008b4584376d0
bkeener <bkeener7@gmail.com>
2022-09-26 11:52:21 -0600

    Test for 100% simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,9 @@ require './lib/item'
 require './lib/auction'
 require './lib/attendee'
 require 'date'
+require 'simplecov'
+
+SimpleCov.start
+require_relative 'attendee_spec'
+require_relative 'auction_spec'
+require_relative 'item_spec'
